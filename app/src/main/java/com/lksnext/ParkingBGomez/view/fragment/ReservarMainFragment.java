4aaa4d9a3e6fca2e2352709cf226d7e28c600bfd
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-04-20 18:00:18 +0200

    feature date selector: added LiveData for selected date, day label, and selector title

--- app/src/main/java/com/lksnext/ParkingBGomez/view/fragment/ReservarMainFragment.java
+++ app/src/main/java/com/lksnext/ParkingBGomez/view/fragment/ReservarMainFragment.java
@@ -1,20 +1,24 @@
 package com.lksnext.ParkingBGomez.view.fragment;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.chip.Chip;
 import com.lksnext.ParkingBGomez.R;
 import com.lksnext.ParkingBGomez.databinding.FragmentReservarMainBinding;
+import com.lksnext.ParkingBGomez.viewmodel.MainViewModel;
 
+import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Locale;
 import java.util.TimeZone;
 import java.util.List;
 import java.util.ArrayList;
@@ -22,7 +26,12 @@ import java.util.ArrayList;
 public class ReservarMainFragment extends Fragment{
 
     private FragmentReservarMainBinding binding;
-    private final List<Integer> day_chip_id = new ArrayList<>(Arrays.asList(R.id.day1, R.id.day2, R.id.day3, R.id.day4, R.id.day5, R.id.day6, R.id.day7));
+    private MainViewModel mainViewModel;
+    private final List<Integer> day_chip_id = new ArrayList<>(Arrays.asList(R.id.day1, R.id.day2,
+            R.id.day3, R.id.day4, R.id.day5, R.id.day6, R.id.day7));
+    private final List<Integer> day_text_id = new ArrayList<>(Arrays.asList(R.id.textView_day1,
+            R.id.textView_day2, R.id.textView_day3, R.id.textView_day4, R.id.textView_day5,
+            R.id.textView_day6, R.id.textView_day7));
 
     @Override
     public View onCreateView(
@@ -32,7 +41,10 @@ public class ReservarMainFragment extends Fragment{
 
         binding = FragmentReservarMainBinding.inflate(inflater, container, false);
 
+        mainViewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
+
         setDay_chip_text();
+        setDay_text();
 
         return binding.getRoot();
 
@@ -53,23 +65,38 @@ public class ReservarMainFragment extends Fragment{
      * The text is the day of the month
      */
     private void setDay_chip_text(){
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone(ZoneId.systemDefault()));
         int day = calendar.get(Calendar.DAY_OF_MONTH);
         for (int chip_id: day_chip_id
              ) {
-            Chip chip = binding.datePicker.findViewById(chip_id);
+            final Chip chip = binding.datePicker.findViewById(chip_id);
             chip.setText(String.valueOf(day));
+            chip.setTag(day); // Set the value to the chip
             calendar.add(Calendar.DAY_OF_MONTH, 1);
             day = calendar.get(Calendar.DAY_OF_MONTH);
             // Add a check change listener to handle the selection
-            chip.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Chip chip = (Chip) v;
-                    chip.setChecked(true);
-                    Log.d("Chip", "Chip clicked"+chip.getText());
-                }
+            chip.setOnClickListener(v -> {
+                final Chip  clickedChip = (Chip) v;
+                mainViewModel.setSelectedDateDay((Integer) clickedChip.getTag());
             });
+        }
+    }
+
+    /**
+     * Set the text of the day text views in the date picker
+     * The text is the day of the month
+     */
+    private void setDay_text(){
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone(ZoneId.systemDefault()));
+        for (int text_id: day_text_id
+             ) {
+            final TextView text = binding.datePicker.findViewById(text_id);
+            String dayOfWeek = calendar.getDisplayName(
+                    Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault());
+            assert dayOfWeek != null;
+            // Set the text to the first letter of the day of the week
+            text.setText(dayOfWeek.substring(0, 1).toUpperCase());
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
         }
     }
 }
--- app/src/main/java/com/lksnext/ParkingBGomez/viewmodel/MainViewModel.java
+++ app/src/main/java/com/lksnext/ParkingBGomez/viewmodel/MainViewModel.java
@@ -1,13 +1,21 @@
 package com.lksnext.ParkingBGomez.viewmodel;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.lksnext.ParkingBGomez.domain.BottomNavState;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Objects;
+
 public class MainViewModel extends ViewModel {
 
-    private final MutableLiveData<BottomNavState> bottomNavState = new MutableLiveData<>(BottomNavState.HOME);
+    private final MutableLiveData<BottomNavState> bottomNavState =
+            new MutableLiveData<>(BottomNavState.HOME);
+    private final MutableLiveData<LocalDate> selectedDate =
+            new MutableLiveData<>(LocalDate.now(ZoneId.systemDefault()));
 
     public MutableLiveData<BottomNavState> getBottomNavState() {
         return bottomNavState;
@@ -17,4 +25,21 @@ public class MainViewModel extends ViewModel {
         bottomNavState.setValue(state);
     }
 
+    public MutableLiveData<LocalDate> getSelectedDate() {
+        return selectedDate;
+    }
+
+    public void setSelectedDate(@NonNull LocalDate date) {
+        selectedDate.setValue(date);
+    }
+
+    /**
+     * Set the selectedDate day of the month to the selected day of the month
+     * @param selectedDateDay the day of the month
+     */
+    public void setSelectedDateDay(int selectedDateDay) {
+        final LocalDate newLocalDate = Objects.requireNonNull(selectedDate.getValue())
+                .withDayOfMonth(selectedDateDay);
+        selectedDate.setValue(newLocalDate);
+    }
 }
