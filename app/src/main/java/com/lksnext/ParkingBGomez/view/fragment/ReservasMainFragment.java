052ac13280af21268e2fe7fef4c7dbde91d51baa
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-05-11 19:08:53 +0200

    feat(reservas): created recyclerview to group reservas by date
    this new recyclerview will have the previous recyclerview nested that will display the reservas

--- app/src/main/java/com/lksnext/ParkingBGomez/view/fragment/ReservasMainFragment.java
+++ app/src/main/java/com/lksnext/ParkingBGomez/view/fragment/ReservasMainFragment.java
@@ -12,14 +12,22 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.lksnext.ParkingBGomez.databinding.FragmentReservasMainBinding;
-import com.lksnext.ParkingBGomez.view.adapter.ReservasAdapter;
+import com.lksnext.ParkingBGomez.domain.Reserva;
+import com.lksnext.ParkingBGomez.view.adapter.ReservasByDayAdapter;
 import com.lksnext.ParkingBGomez.view.decoration.ReservaItemDecoration;
 import com.lksnext.ParkingBGomez.viewmodel.MainViewModel;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ReservasMainFragment extends Fragment{
 
     private FragmentReservasMainBinding binding;
     private MainViewModel mainViewModel;
+    private Map<LocalDate, List<Reserva>> reservasByDay;
 
     @Override
     public View onCreateView(
@@ -29,12 +37,26 @@ public class ReservasMainFragment extends Fragment{
         binding = FragmentReservasMainBinding.inflate(inflater, container, false);
 
         mainViewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
+        reservasByDay = new HashMap<>();
 
         RecyclerView recyclerView = binding.recyclerViewReservas;
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL,false));
 
-        ReservasAdapter adapter = new ReservasAdapter();
-        mainViewModel.getReservasList().observe(getViewLifecycleOwner(), adapter::submitList);
+        mainViewModel.getReservasList().observe(getViewLifecycleOwner(), reservas -> {
+            for (Reserva reserva : reservas) {
+                LocalDate localDate = reserva.fecha().toLocalDate();
+                if (!reservasByDay.containsKey(localDate)) {
+                    reservasByDay.put(localDate, List.of(reserva));
+                } else {
+                    List<Reserva> reservasForDate = new ArrayList<>(reservasByDay.get(localDate));
+                    reservasForDate.add(reserva);
+                    reservasByDay.put(localDate, reservasForDate);
+                }
+            }
+
+        });
+        ReservasByDayAdapter adapter = new ReservasByDayAdapter(reservasByDay);
+
         recyclerView.addItemDecoration(new ReservaItemDecoration(20));
         recyclerView.setAdapter(adapter);
 
---
+++ app/src/main/java/com/lksnext/ParkingBGomez/view/holder/ReservasByDayViewHolder.java
@@ -0,0 +1,68 @@
+package com.lksnext.ParkingBGomez.view.holder;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lksnext.ParkingBGomez.R;
+import com.lksnext.ParkingBGomez.domain.Reserva;
+import com.lksnext.ParkingBGomez.view.adapter.ReservasAdapter;
+import com.lksnext.ParkingBGomez.view.decoration.ReservaItemDecoration;
+
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.List;
+import java.util.Locale;
+
+public class ReservasByDayViewHolder extends RecyclerView.ViewHolder {
+    public ReservasByDayViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public void bind(LocalDate localDate, List<Reserva> reservas) {
+        TextView dateTextView = itemView.findViewById(R.id.reserva_group_day);
+
+        setFormattedDate(localDate, dateTextView);
+
+
+        RecyclerView recyclerView = itemView.findViewById(R.id.reservas_by_day);
+        recyclerView.setLayoutManager(new LinearLayoutManager(itemView.getContext(), LinearLayoutManager.VERTICAL,false));
+        recyclerView.setNestedScrollingEnabled(false);
+
+        ReservasAdapter adapter = new ReservasAdapter();
+
+        adapter.submitList(reservas);
+        recyclerView.addItemDecoration(new ReservaItemDecoration(20));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private static void setFormattedDate(LocalDate localDate, TextView dateTextView) {
+        LocalDate today = LocalDate.now();
+        boolean isToday = today.isEqual(localDate);
+        boolean isYesterday = today.minusDays(1).isEqual(localDate);
+        boolean isTomorrow = today.plusDays(1).isEqual(localDate);
+
+        Context context = dateTextView.getContext();
+        String formattedDate;
+        if (isToday) {
+            formattedDate = context.getString(R.string.today);
+        } else if (isYesterday) {
+            formattedDate = context.getString(R.string.yesterday);
+        } else if (isTomorrow) {
+            formattedDate = context.getString(R.string.tomorrow);
+        } else {
+            // Get the day of the week and month name
+            String dayOfWeek = localDate.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.getDefault());
+            String monthName = localDate.getMonth().getDisplayName(TextStyle.SHORT, Locale.getDefault());
+
+            // Format the date string
+            formattedDate = String.format("%s, %d %s", dayOfWeek, localDate.getDayOfMonth(), monthName);
+        }
+
+        dateTextView.setText(formattedDate);
+    }
+}
