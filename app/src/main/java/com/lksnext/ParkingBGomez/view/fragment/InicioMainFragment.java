e97ec41184f8f4720356c368172612d4f791b79a
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-05-31 23:34:27 +0200

    feat(inicio): add card with google maps intent to Zuatzu

--- app/src/main/java/com/lksnext/ParkingBGomez/view/fragment/InicioMainFragment.java
+++ app/src/main/java/com/lksnext/ParkingBGomez/view/fragment/InicioMainFragment.java
@@ -1,5 +1,7 @@
 package com.lksnext.ParkingBGomez.view.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,10 +13,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import com.lksnext.ParkingBGomez.R;
 import com.lksnext.ParkingBGomez.data.DataRepository;
 import com.lksnext.ParkingBGomez.databinding.FragmentInicioMainBinding;
@@ -43,6 +48,21 @@ public class InicioMainFragment extends Fragment{
             navController.navigate(R.id.action_to_reservas_by_ver_todas_text);
         });
 
+        binding.buttonZuatzuMaps.setOnClickListener(l -> {
+            // Create a Uri from an intent string. Use the result to create an Intent.
+            Uri gmmIntentUri = Uri.parse("https://plus.codes/7XWV+PX,San%20Sebastian");
+
+            // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            // Make the Intent explicit by setting the Google Maps package
+            mapIntent.setPackage("com.google.android.apps.maps");
+
+            // Attempt to start an activity that can handle the Intent
+            startActivity(mapIntent);
+
+
+        });
+
         return binding.getRoot();
 
     }
@@ -67,7 +87,7 @@ public class InicioMainFragment extends Fragment{
 
     private void fetchAndSetReservasFromDB(RecyclerView recyclerView, DataRepository dataRepository, MainActivity activity) {
         LiveData<List<Reserva>> liveData =
-                getReservasOfUserAfterToday(dataRepository, activity);
+                getReservasOfUserAfterToday(recyclerView, dataRepository, activity);
 
         // Is executed when the LiveData object is updated with db data
         liveData.observe(getViewLifecycleOwner(), dbReservasOfUser -> {
@@ -83,7 +103,7 @@ public class InicioMainFragment extends Fragment{
         adapter.submitList(dbReservasOfUser);
     }
 
-    private static LiveData<List<Reserva>> getReservasOfUserAfterToday(DataRepository dataRepository, MainActivity activity) {
+    private static LiveData<List<Reserva>> getReservasOfUserAfterToday(View view, DataRepository dataRepository, MainActivity activity) {
         return dataRepository.getReservasOfUserAfterToday("usuario", activity, new Callback() {
             @Override
             public void onSuccess() {
@@ -93,6 +113,9 @@ public class InicioMainFragment extends Fragment{
             @Override
             public void onFailure() {
                 Log.d("ReservaCardAdapter", "Failure :(");
+                Snackbar.make(view,
+                        "La aplicación no ha podido cargar tus proximas reservas. Intentalo de nuevo más tarde.",
+                        BaseTransientBottomBar.LENGTH_LONG).show();
             }
         });
     }
